5ba0ebd9b8be9f09c7c11066db8baa28773b2253
BijonTalukder <bijontalukder1247@gmail.com>
2025-06-22 15:29:22 +0600

    update

--- apps/restapi/src/index.ts
+++ apps/restapi/src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello rest")
+import {CreateNewUserUseCase} from "@ocmo/core/use-cases/register-user"
+import {InMemoryUserRepository} from  "@ocmo/database"
+
+import express,{Application} from 'express'
+
+const app:Application=express()
+const createUser= new CreateNewUserUseCase(new InMemoryUserRepository)
+
+
+createUser.execute({
+    name:"jhone doe",
+    password:"adfsd",
+    phone:"01231231",
+    role:"user"
+})
+app.listen(300,()=>{
+
+})
